8f4363270970ffc689394c782e5fccb0ca21bd5b
Arpita Das <arpita1477@gmail.com>
2026-03-24 01:47:41 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday6/Time_Machine.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    if(x>=2025 && x<=2050){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
