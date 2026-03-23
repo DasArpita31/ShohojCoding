7bd05aa2dedc518d9e0f5a1b3b8b8c836f91f1ba
Arpita Das <arpita1477@gmail.com>
2026-03-24 01:35:08 +0600

    Create Conquer_the_Fest.cpp

---
+++ CodechefCamp/Codechefday3/Conquer_the_Fest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,b;
+    cin>>n>>b;
+    if((b*10)<=n){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
