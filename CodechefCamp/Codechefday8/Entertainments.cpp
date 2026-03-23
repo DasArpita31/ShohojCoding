2e43e5140aaca13ee250a008f6fdac29ff942617
Arpita Das <arpita1477@gmail.com>
2026-03-24 01:51:58 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday8/Entertainments.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n<=5) cout<<n*200<<endl;
+    else cout<<1000<<endl;
+}
---
+++ CodechefCamp/Codechefday8/Too_Much_Homework.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x+(y*10)>=100) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
