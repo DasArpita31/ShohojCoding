a0199a4868fee55bfb7ab01d307f9bb5acdf841d
Arpita Das <arpita1477@gmail.com>
2026-03-25 21:24:43 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday14/Apples_and_Oranges.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,a,b;
+    cin>>x>>a>>b;
+    if((a+b)<=x) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
---
+++ CodechefCamp/Codechefday14/Ticket_Fine.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,q,p;
+        cin>>x>>q>>p;
+        cout<<((q-p)*x)<<endl;
+    }
+}
