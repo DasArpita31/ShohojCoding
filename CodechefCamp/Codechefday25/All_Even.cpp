df1c5d83c10b65586b28da10e9a66f65f4702a73
Arpita Das <arpita1477@gmail.com>
2026-03-26 00:15:31 +0600

    Add All_Even.cpp to check sum of even numbers

---
+++ CodechefCamp/Codechefday25/All_Even.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n],sum=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        if(sum%2==0){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
