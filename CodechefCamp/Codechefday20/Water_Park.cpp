3a8c32721e50011661c3726d2184ab8c54114d04
Arpita Das <arpita1477@gmail.com>
2026-03-25 23:51:00 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday20/Water_Park.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int w,h;
+    cin>>w>>h;
+    if(w>=60 && h<=130){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
