c75d41b5ce3d13c13f18e8fa6a4dde7bbe23e25c
Arpita Das <arpita1477@gmail.com>
2026-03-25 22:11:35 +0600

    Create ATM.cpp

---
+++ CodechefCamp/Codechefday17/ATM.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    double y;
+    cin>>x>>y;
+    if(x%5==0 && y>=x+0.5)
+    y=y-x-0.5;
+    cout<<fixed<<setprecision(2)<<y<<endl;
+}
