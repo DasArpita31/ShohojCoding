7fa3a5392ca75927b669397baa74491d4d60fb37
Arpita Das <arpita1477@gmail.com>
2026-03-25 22:16:40 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday19/Merry_Christmas.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    if(x<=2) cout<<1<<endl;
+    else if(x<=6) cout<<2<<endl;
+    else cout<<3<<endl;
+}
---
+++ CodechefCamp/Codechefday19/New_Pro_Coder.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    double a=(double)n/2;
+    if(a>m) cout<<"PRO"<<endl;
+    else cout<<"NEWBIE"<<endl;
+}
