325f0cde706ccb0c7ad788b4b816221df79aa629
Arpita Das <arpita1477@gmail.com>
2026-03-26 12:48:18 +0600

    Add Card_Game.cpp for card game logic

---
+++ CodechefCamp/Codechefday30/Card_Game.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(x%2==0) cout<<(n/2)-1<<endl;
+        else cout<<((n+1)/2)-1<<endl;
+    }
+}
