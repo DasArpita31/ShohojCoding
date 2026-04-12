9a04aa081d419d16cd3752893f2a37609d7ac209
Arpita Das <arpita1477@gmail.com>
2026-04-12 22:51:39 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday33/Minimum_number_of_coins.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int x;
+        cin>>x;
+
+        if(x%5!=0){
+            cout<<-1<<endl;
+        }
+        else{
+            int c=(x/10)+((x%10)/5);
+            cout<<c<<endl;
+        }
+    }
+}
