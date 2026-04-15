2323b319f86bbdafa526f6dbc6bb9809320b0dd2
Arpita Das <arpita1477@gmail.com>
2026-04-15 23:30:16 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday35/Too_Many_Oranges.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(k>=10*n && k<=12*n) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+	}
+}
