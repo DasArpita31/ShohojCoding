44ff3cff80cb6ae58b91158831b6c3f444073369
Arpita Das <arpita1477@gmail.com>
2026-04-19 21:44:17 +0600

    Add files via upload

---
+++ CodechefCamp/Codechefday39/Minimum_number_of_Flips.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int x,pos=0,neg=0;
+
+        for (int i=0;i<n;i++){
+            cin>>x;
+            if (x==1) pos++;
+            else neg++;
+        }
+
+        if (n%2!=0){
+            cout<<-1<<endl;
+        } else {
+            cout<<abs(pos-neg)/2<<endl;
+        }
+	}
+
+}
